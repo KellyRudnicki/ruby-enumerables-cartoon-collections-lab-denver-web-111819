522b490d8c898f848c96fdb2ede7a80ea9c5a8da
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-13 03:30:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,4 +18,8 @@ def find_the_cheese(array)# code an argument here
   
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  if array.include? cheese_types
+    return 
+  end 
 end
